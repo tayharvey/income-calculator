6ea298c2517e27f35beed99a05b715ac4cdecd4a
ThomasLech <thomas@rootxnet.com>
2021-07-06 23:35:03 +0700

    Left Navbar Design Upgrade

--- frontend/src/components/navbar/LeftNavBar.js
+++ frontend/src/components/navbar/LeftNavBar.js
@@ -2,26 +2,32 @@ import React from "react";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import {NavBarItem} from "./NavBarItem";
-import {Divider} from "@material-ui/core";
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
-import VpnKeyIcon from '@material-ui/icons/VpnKey';
-import PeopleIcon from '@material-ui/icons/People';
+import {ReactComponent as UserList} from '../../icons/user-list.svg';
+import {ReactComponent as UserListActive} from '../../icons/user-list-active.svg';
+import {ReactComponent as AdminList} from '../../icons/admin-list.svg';
+import {ReactComponent as AdminListActive} from '../../icons/admin-list-active.svg';
+import {ReactComponent as Api} from '../../icons/api.svg';
+import {ReactComponent as ApiActive} from '../../icons/api-active.svg';
+import {ReactComponent as Logout} from '../../icons/logout.svg';
 import "../stylesheets/styles-navigation.css"
 
 
 export const LeftNavBar = () => {
+
+  const isActive = (url) => window.location.pathname === url;
+
   return <Drawer variant="permanent" anchor={"left"} style={{
     flexShrink: 0,
   }}>
     <List className='menu-container'>
-      <NavBarItem link="/users/" title="User List" icon={<PeopleIcon/>}/>
+      <NavBarItem link="/users/" title="User List"
+                  icon={isActive("/users/") ? <UserListActive/> : <UserList/>}/>
       <NavBarItem link="/accounts/" title="Admin List"
-                  icon={<SupervisorAccountIcon/>}/>
-      <NavBarItem link="/api-keys/" title="API" icon={<VpnKeyIcon/>}/>
-      <Divider className="padded-divider"/>
-      <NavBarItem link="/auth/logout/" title="Logout"
-                  icon={<PowerSettingsNewIcon/>}/>
+                  icon={isActive("/accounts/") ? <AdminListActive/> : <AdminList/>}/>
+      <NavBarItem link="/api-keys/" title="API"
+                  icon={isActive("/api-keys/") ? <ApiActive/> : <Api/>}/>
+      <NavBarItem link="/auth/logout/" title="Log Out" align="bottom"
+                  icon={<Logout/>}/>
     </List>
   </Drawer>
 }
--- frontend/src/components/navbar/NavBarItem.js
+++ frontend/src/components/navbar/NavBarItem.js
@@ -3,8 +3,8 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import "../stylesheets/styles-navigation.css"
 
-export const NavBarItem = ({link, title, icon}) => {
-  return <ListItem>
+export const NavBarItem = ({link, title, icon, align}) => {
+  return <ListItem style={align === "bottom" ? {position: "absolute", "bottom": 20} : {}}>
     {icon}
     <NavLink to={link} className="navbar-link">
       {title}
