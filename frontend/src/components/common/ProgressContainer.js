cc8fbc6855a05eec0ba303da8e8767225f518661
ThomasLech <thomas@rootxnet.com>
2021-07-08 18:20:55 +0700

    Improved the appearance of the loading spinner

--- frontend/src/components/common/ProgressContainer.js
+++ frontend/src/components/common/ProgressContainer.js
@@ -1,9 +1,51 @@
-import {CircularProgress} from "@material-ui/core";
+import {CircularProgress, makeStyles} from "@material-ui/core";
 import React from "react";
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "relative",
+  },
+  bottom: {
+    color: "#e7e7ff"
+  },
+  top: {
+    color: "#6564F5",
+    animationDuration: "550ms",
+    position: "absolute",
+    left: 0,
+  },
+  circle: {
+    strokeLinecap: "round",
+  },
+}));
+
 export const ProgressContainer = () => {
-  return <div className='loader-div'>
-    Loading data...
-    <CircularProgress size={50}/>
-  </div>
+  const classes = useStyles();
+
+  return (
+    <div className="loader-div">
+      Loading data...
+
+      <div className={classes.root}>
+        <CircularProgress
+          variant="determinate"
+          className={classes.bottom}
+          size={50}
+          thickness={4}
+          value={100}
+        />
+        <CircularProgress
+          variant="indeterminate"
+          disableShrink
+          className={classes.top}
+          classes={{
+            circle: classes.circle,
+          }}
+          size={50}
+          thickness={4}
+        />
+      </div>
+    </div>
+  );
 }
